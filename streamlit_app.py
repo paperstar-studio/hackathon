5085b92b938d9a9ea946f2a58576c1e9b632cc29
paperstar-studio <166758222+paperstar-studio@users.noreply.github.com>
2026-04-13 19:26:14 +0200

    initial setup and info

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,33 @@
+import sys
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+
+st.title("Hackathon Prep")
+st.write(f"using python version: {sys.version}")
+
+st.markdown(""" 
+### main hackathon website
+https://www.sustainista.net/hackathon
+
+may 15th 16:00 - may 17th 12:30
+
+
+### 3 hackathon challenges in 2024
+1. Combating Political Fake News with AI
+2. Uncovering New Business Horizons with AI
+3. ESG Insight AI Accelerator
+
+source: https://www.sustainista.net/blog-1/2019/3/11/on-letting-go-ft8xd
+
+--- 
+
+ """)
+
+st.image("email.png")
+
+st.markdown(""" 
+### current ideas
+
+1. 
+2. 
+    """)
