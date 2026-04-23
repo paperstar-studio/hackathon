86d9b3a59e270f046e0108c36a4540d54a43b221
paperstar-studio <166758222+paperstar-studio@users.noreply.github.com>
2026-04-23 18:29:07 +0200

    Create first_board_meeting.py

---
+++ pages/first_board_meeting.py
@@ -0,0 +1,27 @@
+import sys
+import streamlit as st
+
+
+st.title("1st board meeting [  ]")
+st.write(f"using python version: {sys.version}")
+
+st.markdown(""" 
+### Who gets to make what decisions, primarely?
+- DESIGN: Emma
+- probably joint presenter
+
+
+# bussiness idea
+
+### revenue from 
+	1. industry
+	2. consumers
+	3. governmental funding
+
+
+### potential risks:
+	* make money first of all
+	* what are you going to mitigate regulatory body risk > don't spend all your money on lawyers
+
+
+	""")
